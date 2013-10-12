225ec96f83477621931d5d2167b354aeebdd8b1b
al.shenia <al.shenia@gmail.com>
2013-10-12 15:58:31 +0300

    devise views

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,9 @@
-require 'spec_helper'
-
 describe Post do
+  it { should have_many :comments }
+
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+
+  it { should have_db_column(:title).of_type(:string) }
+  it { should have_db_column(:body).of_type(:text) }
 end
