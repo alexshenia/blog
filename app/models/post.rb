7eb1f6c0b6195d26a63dccb1532072854da7f0d2
al.shenia <al.shenia@gmail.com>
2013-10-12 16:57:04 +0300

    User valid

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
-  has_many :comments
 
-  validates_presence_of :title, :body
+  has_many :comments
+  belongs_to :user
+
+  validates_presence_of :title, :body, :user
 end
